82b63d95c72463c9bc3816150bd57a5b4819721f
Jonathan Pillajo <pandc19@outlook.com>
2024-02-10 12:15:18 -0500

    fin sección 28

---
+++ src/app/app.config.ts
@@ -0,0 +1,23 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+
+import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(
+      routes,
+      withViewTransitions({
+        skipInitialTransition: true,
+        // onViewTransitionCreated(transitionInfo) {
+        //   console.log(transitionInfo);
+        // }
+      }),
+    ),
+
+    importProvidersFrom(
+      HttpClientModule,
+    ),
+  ]
+};
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './sidemenu.component.html',
+  styles: ``
+})
+export class SidemenuComponent {
+
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter(route => route && route.path)
+    .filter(route => !route.path?.includes(':'));
+
+  constructor() {
+
+    // const dashboardRoutes = routes
+    //   .map(route => route.children ?? [])
+    //   .flat()
+    //   .filter(route => route && route.path)
+    //   .filter(route => !route.path?.includes(':'));
+
+    // console.log(dashboardRoutes);
+  }
+
+}
